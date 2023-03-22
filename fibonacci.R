be71b27b5e1d4252e978fe67e44ae8877ed865ec
siddamaheshwar <113339062+siddamaheshwar@users.noreply.github.com>
2023-03-22 09:43:16 +0530

    Create fibonacci.R

---
+++ fibonacci.R
@@ -0,0 +1,9 @@
+fibonacci_recursive <- function(n) {
+  if (n <= 1) {
+    return(n)
+  } else {
+    return(fibonacci_recursive(n-1) + fibonacci_recursive(n-2))
+  }
+}
+print(fibonacci_recursive(6))
+
